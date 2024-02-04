d66279653ef9b0766cbd4ae1438273250f285adc
JakobHMills <133824314+JakobHMills@users.noreply.github.com>
2024-02-04 11:17:46 -0700

    Week 3 update

--- app/page.js
+++ app/page.js
@@ -4,9 +4,10 @@ const AppPage = () => {
   return (
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-
-      {/* Wrap the content to make it clickable with Link */}
+      {}
       <Link href="/week-2">Go to Week 2 Page</Link>
+      <p/>
+      <Link href="/week-3">Go to Week 3 Page</Link>
     </div>
   );
 };
